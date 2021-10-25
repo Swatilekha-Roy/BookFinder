762cdcc17ca1a93614f7677db49ea54beec57b7b
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-10-25 13:08:23 +0530

    added header and footer

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,16 @@
-/* All browser side scripts run here */
+/* Scroll to top arrow */
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {
+    $("#return-to-top").fadeIn(200);
+  } else {
+    $("#return-to-top").fadeOut(200);
+  }
+});
+$("#return-to-top").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    500
+  );
+});
